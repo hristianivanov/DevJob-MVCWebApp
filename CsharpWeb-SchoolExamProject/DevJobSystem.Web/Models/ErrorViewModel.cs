377edd01b7c0dd08098b499f984afcee960b45fb
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-09-20 09:18:17 +0300

    Organizing project architecture

---
+++ CsharpWeb-SchoolExamProject/DevJobSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevJobSystem.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
