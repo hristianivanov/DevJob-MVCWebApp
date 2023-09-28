537d2a56dcd39ffee982b946640301ed015e7d22
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-09-28 09:47:39 +0300

    Add clear services class and interfaces

---
+++ DevJobSystem/DevJobSystem/Services/Interfaces/IProgrammerService.cs
@@ -0,0 +1,12 @@
+﻿namespace DevJobSystem.Services.Interfaces
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	internal interface IProgrammerService
+	{
+	}
+}
